fe41bd1f66983e3e5b52353d6836042a2e1638d2
Perry <perrchick@gmail.com>
2016-01-19 17:49:57 +0200

    1st commit

---
+++ SomeApp/SomeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SomeApp
+//
+//  Created by Perry on 1/19/16.
+//  Copyright © 2016 PerrchicK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
