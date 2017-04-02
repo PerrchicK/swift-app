34f2a1c3e64af7e1d6d664787737f5ddbe348363
Perry Shalom <perry@nested-group.com>
2017-04-02 07:21:54 +0300

    Removed file: TicTabToeGame, so the game's logic is turned off

--- SomeApp/SomeApp/Classes/ViewControllers/CollectionContainerViewController.swift
+++ SomeApp/SomeApp/Classes/ViewControllers/CollectionContainerViewController.swift
@@ -9,23 +9,15 @@
 import Foundation
 import UIKit
 
-class CollectionContainerViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, TicTabToeGameDelegate {
+class CollectionContainerViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let NumberOfRows = TicTabToeGame.Configuration.RowsCount // X - number of section
-    let NumberOfColumns = TicTabToeGame.Configuration.ColumnsCount // Y - number of items in section
+    let NumberOfRows = 3 // X - number of section
+    let NumberOfColumns = 3 // Y - number of items in section
     let TileMargin = CGFloat(5.0)
 
-    var game: TicTabToeGame!
     var isGameEnabled = true
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        game = TicTabToeGame()
-        game.delegate = self
-    }
 
     func reloadGame() {
         collectionView.reloadData()
@@ -36,7 +28,7 @@ class CollectionContainerViewController: UIViewController, UICollectionViewDataS
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
         // This will: (1) dequeue the cell, if it doesn't exist it will create one. (2) will cast it to our custom cell. (3) will assert that the casting is legal.
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: className(CollectionContainerCell), for: indexPath) as! CollectionContainerCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: className(CollectionContainerCell.self), for: indexPath) as! CollectionContainerCell
 
         cell.configCell()
 
@@ -54,24 +46,6 @@ class CollectionContainerViewController: UIViewController, UICollectionViewDataS
 
     // MARK: - UICollectionViewDelegate
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let cell = collectionView.cellForItem(at: indexPath) as! CollectionContainerCell
-
-        let currentPlayerMark = game.currentPlayer.rawValue
-        if game.playerMadeMove(indexPath.section, column: indexPath.row) {
-            // Move confirmed
-            cell.placeMark(currentPlayerMark)
-        }
-    }
-
-    func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
-//        collectionView.invalidateLayout() performBatch...
-    }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
         let rowsCount = CGFloat(NumberOfColumns)
         let dimentions = collectionView.frame.height / rowsCount - (rowsCount * TileMargin * 0.8)
@@ -81,17 +55,6 @@ class CollectionContainerViewController: UIViewController, UICollectionViewDataS
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
         return UIEdgeInsetsMake(TileMargin, TileMargin, TileMargin, TileMargin)
     }
-
-    // MARK: - TicTabToeGameDelegate
-
-    func ticTabToeGame(_ game: TicTabToeGame, finishedWithWinner winner: TicTabToeGame.Player) {
-        ToastMessage.show(messageText: "winner: \(winner)")
-        isGameEnabled = false
-    }
-
-    func isGameEnabled(_ game: TicTabToeGame) -> Bool {
-        return isGameEnabled
-    }
 }
 
 class CollectionContainerCell: UICollectionViewCell {
@@ -99,12 +62,7 @@ class CollectionContainerCell: UICollectionViewCell {
     @IBOutlet weak var playerMarkLabel: UILabel!
 
     func configCell() {
-        📘("configuring cell")
         self.backgroundColor = UIColor.red
         self.playerMarkLabel.text = ""
-    }
-
-    func placeMark(_ mark: String) {
-        self.playerMarkLabel.text = mark
     }
 }
