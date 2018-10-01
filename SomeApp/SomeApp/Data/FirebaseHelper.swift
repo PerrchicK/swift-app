5fc70c0e4a4f181ffa8252bfe6bdb64087778080
Perry Shalom <perry@nested-group.com>
2018-10-01 03:16:29 +0300

    Added a cute way to let FCF act like a NodeJS server and respond to POST-like requests

--- SomeApp/SomeApp/Data/FirebaseHelper.swift
+++ SomeApp/SomeApp/Data/FirebaseHelper.swift
@@ -18,8 +18,10 @@ class FirebaseHelper {
     private static let rootRef: DatabaseReference = Database.database().reference()
     private struct Keys {
         static let Users: String = "Users"
+        static let Requests: String = "Requests"
     }
 
+    static var requestsCounter = 0
     static let dbUrl: String = {
         return (GoogleServiceInfoPlistContent["DATABASE_URL"] as? String) ?? ""
     }()
@@ -97,6 +99,90 @@ class FirebaseHelper {
             initialize()
             createUserNode(user: user)
         }
+    }
+
+    static func performPseudoPostRequest(path: String, data requestData: [String:Any], completion: @escaping ([String:Any]?) -> ()) {
+        guard let deviceId = UIDevice.current.identifierForVendor?.description else {
+            completion(nil)
+            return
+        }
+        let timestamp = Int64(Date().timeIntervalSince1970 * 1000)
+        let requestId = "\(deviceId)_\(FirebaseHelper.requestsCounter)_\(timestamp)"
+        FirebaseHelper.requestsCounter += 1
+        let requestReference = rootRef.child(Keys.Requests).child("\(requestId)")
+        
+        requestReference.observe(DataEventType.childAdded) { (dataSnapshot) in
+            guard dataSnapshot.key == "responseData" else { return }
+            guard let responseDataContainer = dataSnapshot.value as? [String:Any] else { completion(nil); return }
+            guard let responseData = responseDataContainer["data"] as? [String:Any] else { completion(nil); return }
+
+            📘("Request ID: \(requestId), request data: \(requestData), response data: \(responseData)")
+            requestReference.removeAllObservers()
+            requestReference.removeValue()
+            completion(responseData)
+        }
+
+        var request: [String:Any] = [:]
+        request["requestData"] = requestData
+        request["requestPath"] = path
+
+        requestReference.setValue(request) { (requestSentError, ref) in
+            //📘(ref)
+        }
+        
+        /*
+         
+         The NodeJS code that handles this "POST request" in FCF style:
+         
+         /**
+         * Triggered from a message on a Cloud Pub/Sub topic.
+         *
+         * @param {!Object} event The Cloud Functions event.
+         * @param {!Function} The callback function.
+         */
+         exports.requests = functions.database.ref('/Requests/{requestId}').onWrite(event => {
+         // Only edit data when it is first created.
+         if (event.data.previous.exists()) return false;
+         // Exit when the data is deleted.
+         if (!event.data.exists()) return false;
+         
+         const dataSnapshot = event.data.val();
+         if (!event || !event.params || !event.params.requestId) {
+         console.error("missing some parameters in " + (event ? JSON.stringify(event) : "event"));
+         return false;
+         }
+         
+         //dataSnapshot.requestData |= {}; ??
+         if (!dataSnapshot.requestData) {
+         dataSnapshot.requestData = {};
+         }
+         if (!dataSnapshot.requestPath) {
+         dataSnapshot.requestPath = "";
+         }
+         return handleRequest(event.data.key, dataSnapshot.requestPath, dataSnapshot.requestData, event.data.ref);
+         });
+         
+         function handleRequest(requestId, path, requestData, reference) {
+         function respond(data) {
+         let timestampString = generateTimestamp() + "";
+         reference.child("responseData").set({"data": data, "receivedPath": path, "timstamp": timestampString})
+         //admin.database().ref().child("requests").child(requestId).set(dataSnapshotValue);
+         }
+         
+         // Send response
+         switch (path) {
+         case "sendNotification":
+         break;
+         case "fetchContacts": {
+         let numbers = {"+972501234567":"perrchick"};
+         respond({"results":numbers});
+         break;
+         }
+         }
+         
+         return true;
+         }
+         */
     }
 
 }
